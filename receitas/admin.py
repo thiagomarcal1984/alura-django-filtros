b6cbedbf1bde97722e72852b8f8270861f6da140
Thiago Marçal <thiagomarcal1984@gmail.com>
2022-05-10 19:47:29 -0300

    ModelAdmin: busca, filtros e paginação.

--- receitas/admin.py
+++ receitas/admin.py
@@ -6,5 +6,8 @@ class ListandoReceitas(admin.ModelAdmin):
     # Opções disponíveis em https://docs.djangoproject.com/en/4.0/ref/contrib/admin/#modeladmin-options
     list_display = ('id', 'nome_receita', 'categoria')
     list_display_links = ('id', 'nome_receita')
+    search_fields = ('nome_receita',) # Busca num campo no topo.
+    list_filter = ('categoria',) # Filtros em um painel à direita.
+    list_per_page = 2 # Paginação após a tabela.
 
 admin.site.register(models.Receita, ListandoReceitas)
