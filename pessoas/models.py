ab02adff6b039c2b8328e0264f877716dfe7abfb
Thiago Marçal <thiagomarcal1984@gmail.com>
2022-05-10 21:06:15 -0300

    Nome da pessoa na receita: redefinindo o __str__.

--- pessoas/models.py
+++ pessoas/models.py
@@ -3,3 +3,7 @@ from django.db import models
 class Pessoa(models.Model):
     nome = models.CharField(max_length=200)
     email = models.EmailField(max_length=200)
+
+    def __str__(self):
+        return self.nome
+        
